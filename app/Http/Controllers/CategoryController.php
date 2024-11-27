e560b52469226805a0f585703055b87c3e320edd
Matin Soleymani <matinsoleymni@gmail.com>
2024-11-28 01:24:47 +0330

    add: category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $user = $request->user();
+        $categories = Category::where("user_id" , "=" , $user->id)->get();
+
+        return response()->json(["categories" => $categories]);
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $category = Category::create([
+            "name" => $request->name,
+            "user_id" => $request->user()->id
+        ]);
+
+        return response()->json(['category' => $category] , 201);
+    }
+
+    public function edit(Request $request , string $id)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        try {
+            $category = Category::findOrFail($id);
+            $category->update([
+                "name" => $request->name
+            ]);
+
+            return response()->json(['category' => $category , "message" => "updated"]);
+
+        }catch (Exception $e)
+        {
+            Log::error($e->getMessage());
+            return response()->json(["message" => "request failed."] , 400);
+        }
+    }
+
+    public function show(Request $request , string $id)
+    {
+        try {
+            $category = Category::findOrFail($id);
+            return response()->json(['category' => $category]);
+        }catch (Exception $e)
+        {
+            Log::error($e->getMessage());
+            return response()->json(["message" => "request failed."] , 400);
+        }
+    }
+
+    public function delete(Request $request , string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response()->json(['message' => "deleted"]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,11 @@ Route::post("/login" , [AuthController::class , "login"]);
 Route::post("/register" , [AuthController::class , "register"]);
 Route::post("/logout" , [AuthController::class , "logout"])->middleware("auth:sanctum");
 Route::post("/me" , [AuthController::class , "me"])->middleware("auth:sanctum");
+
+Route::prefix("category")->middleware("auth:sanctum")->controller(CategoryController::class)->name("category.")->group(function(){
+    Route::get("/" , "index")->name("index");
+    Route::get("/{id}" , "show")->name("show");
+    Route::post("/create" , "create")->name("create");
+    Route::put("/edit/{id}" , "edit")->name("edit");
+    Route::delete("/delete/{id}" , "delete")->name("delete");
+});
